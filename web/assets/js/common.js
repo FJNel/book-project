277dc4ba727971c4b41fbd6509f4c529d3fc5e1e
FJNel <u24594475@tuks.co.za>
2025-10-01 14:05:34 +0200

    feat: Increase loading modal display duration to enhance user experience

--- web/assets/js/common.js
+++ web/assets/js/common.js
@@ -123,7 +123,7 @@ async function initializeApp() {
     } finally {
         //This block will always execute, hiding the now-visible modal.
         //Simulate a short delay to ensure users see the loading modal briefly
-        await new Promise(resolve => setTimeout(resolve, 500));
+        await new Promise(resolve => setTimeout(resolve, 5000));
         pageLoadingModal.hide();
         console.log('[Initialization] Page loading modal hidden.');
     }
